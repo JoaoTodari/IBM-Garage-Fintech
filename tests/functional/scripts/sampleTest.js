7e75c2f2327aa41dc94a808a14cfa2e81ebe551a
GEANE CRISTINA DE MELO <geanecm@br.ibm.com>
2017-12-19 11:09:02 -0200

    testes1

--- tests/functional/scripts/sampleTest.js
+++ tests/functional/scripts/sampleTest.js
@@ -4,8 +4,8 @@ module.exports = {
       .url('http://fintechgaragelab-test.mybluemix.net/')
       .waitForElementVisible('#saiba-maisLink', 60000, true,
         function(){}, 'Waiting for button to become visible')
-      /*.click('#saiba-maisLink')
-      .waitForElementPresent('#rodizio h2', 60000)*/
+      .click('#saiba-maisLink')
+      .waitForElementPresent('#rodizio h2', 60000)
       .pause(5000)
       .end();
   }
